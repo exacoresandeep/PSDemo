2d9528ddcb3e7752469db5e9ed146fd1cad7fa10
exacoresandeep <sandeep.ps@exacoreitsolutions.com>
2026-01-16 17:05:49 +0530

    add date in influencer visit followup updated

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -1347,7 +1347,7 @@ class LeadController extends Controller
                         'created_at' => ($visit->status === 'Follow Up' || $visit->status === 'Won' || $visit->status === 'Lost')
                             ? optional($visit->updated_at)->format('d/m/Y h:i A')
                             : optional($visit->created_at)->format('d/m/Y h:i A'),
-                        'follow_up_date' => optional($visit->follow_up_date)->format('d/m/Y h:i A') ?? null,
+                        'follow_up_date' => optional($visit->created_at)->format('d/m/Y h:i A') ?? null,
                         'status' => $visit->status,
                     ];
                 });
@@ -1423,7 +1423,7 @@ class LeadController extends Controller
                 'created_at' => ($visit->status === 'Follow Up' || $visit->status === 'Won' || $visit->status === 'Lost')
                     ? optional($visit->updated_at)->format('d/m/Y h:i A')
                     : optional($visit->created_at)->format('d/m/Y h:i A'),
-                'follow_up_date' => optional($visit->follow_up_date)->format('d/m/Y h:i A') ?? null,
+                'follow_up_date' => optional($visit->created_at)->format('d/m/Y h:i A') ?? null,
                 'status' => $visit->status,
             ];
         });
